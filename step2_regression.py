77051ac1cc99a15f7b7e4231cab8ea75368582a6
xiamengmeng123 <67639807+xiamengmeng123@users.noreply.github.com>
2024-11-06 16:41:31 +0800

    Add files via upload

---
+++ step2_regression.py
@@ -0,0 +1,17 @@
+import pandas as pd 
+from unimol_tools import MolTrain,MolPredict
+
+validation_path = ('./csv/erfenlei_selected_million.csv')
+data = pd.read_csv(validation_path,usecols=['molecule_id','SMILES'])#'molecule_id',
+data.columns = ["molecule_id","SMILES"]
+
+
+clf = MolPredict(load_model='./clf_interaction')
+
+validation_pred = clf.predict(validation_path)
+data['pred'] = validation_pred
+clf.save_predict(data=data,dir='./csv/',prefix='step2_prediction_results')
+
+
+
+
---
+++ step3_regression.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+from unimol_tools import MolTrain,MolPredict
+
+validation_path = ('./csv/step2_prediction_results.predict.million.csv')
+data = pd.read_csv(validation_path,usecols=['molecule_id','SMILES','pred']) #'molecule_id',
+
+filtered_data = data[data['pred'] > 200]
+filter_path = './csv/filtered_data.csv'
+filtered_data.to_csv(filter_path,index=False)
+
+clf = MolPredict(load_model='./clf_fep')
+
+filtered_data['fep'] = clf.predict(filter_path)
+filtered_data = filtered_data.sort_values(by='fep').reset_index(drop=True)
+clf.save_predict(data=filtered_data,dir='./csv/',prefix='step3_prediction_results')
